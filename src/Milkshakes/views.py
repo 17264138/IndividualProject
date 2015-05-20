610f13d49f0cd32e5167db031aae4caeff71c2f6
17264138 <17264138@sun.ac.za>
2015-05-20 19:41:20 +0200

    app now works with email objects

--- src/Milkshakes/views.py
+++ src/Milkshakes/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 
-def home(request):
-	context = {}
-	template = "home.html"
-
-	return render(request, template, context)
+#def home(request):
+#	context = {}
+#	template = "home.html"
+#
+#	return render(request, template, context)
